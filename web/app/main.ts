1769c225f2ccb454c90f0f15bd1775b6a44a6344
Dennis Wethmar <Dennis.Wethmar@macaw.nl>
2019-04-17 10:16:23 +0200

    Some clean up

--- web/app/main.ts
+++ web/app/main.ts
@@ -1,6 +1,7 @@
-import * as PIXI from "pixi.js";
-import {Game} from "./game/game";
-import {Ticker} from "pixi.js";
+import * as PIXI from 'pixi.js';
+import { Game } from './game/game';
+import { Ticker } from 'pixi.js';
+import { version } from './version';
 
 // import "./wasm";
 
@@ -9,7 +10,7 @@ const viewPort = { width: 750, height: 500 };
 let app = new PIXI.Application(viewPort);
 document.body.appendChild(app.view);
 
-app.renderer.backgroundColor = 0xF5F5F5;
+app.renderer.backgroundColor = 0xf5f5f5;
 app.loader.load(setup);
 
 let game: Game = new Game(app);
@@ -17,11 +18,11 @@ let game: Game = new Game(app);
 // setup ticker
 const ticker = new Ticker();
 ticker.add((delta: number) => {
-    game.update(delta);
-    document.title = `SODA FPS:${Math.floor(app.ticker.FPS)}`;
+	game.update(delta);
+	document.title = `SODA ${version} FPS:${Math.floor(app.ticker.FPS)}`;
 });
 ticker.start();
 
 function setup() {
-    console.log("Setup");
+	console.log('Setup');
 }
--- web/app/wasm.ts
+++ web/app/wasm.ts
@@ -1,10 +1,10 @@
-import "../vendor/wasm/wasm_exec.js"
+import '../vendor/wasm/wasm_exec.js';
 
 declare var Go;
 
 const go = new Go();
 
 // @ts-ignore
-WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
-    go.run(result.instance);
+WebAssembly.instantiateStreaming(fetch('main.wasm'), go.importObject).then((result) => {
+	go.run(result.instance);
 });
